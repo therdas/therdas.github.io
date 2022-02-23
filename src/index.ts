863ba532237a16506c02612112042bdc493c2c6e
therdas <therealrdas@gmail.com>
2022-02-24 02:28:17 +0530

    Added Typewriter effect

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
+//Import main Stylesheet
+import './style.sass'
+
+const typeTarget: string = "typewrite-text"
+
+
+
 function printHello() {
     console.log("print Hello World");
 }
 
-export {printHello};
+function typewrite() {
+    const elem: Element | null = document.querySelector('#' + typeTarget)?.querySelector('.typed') ?? null;
+
+    if(elem == null)
+        return;
+
+    const toType: string = elem?.textContent ?? "";
+    elem.textContent = "";
+    
+    var index: number = 0
+    
+    var typeOne = (_: Event) => {
+        elem.textContent += toType == null ? "" : toType[index++];
+        if(index < toType.length)
+            window.setTimeout(typeOne, 70);
+    }
+
+    window.setTimeout(typeOne, 70);
+}
+
+window.onload = () => typewrite()
+
+export {printHello, typewrite};
